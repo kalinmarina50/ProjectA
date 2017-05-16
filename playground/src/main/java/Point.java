7ecee57dfaeee9cc436b6b467d60f238e4737be6
kalinmarina50 <marinacalin@marinas-air.attlocal.net>
2017-05-16 14:49:06 -0700

    Changed name to ProjectA; added code

--- playground/src/main/java/Point.java
+++ playground/src/main/java/Point.java
@@ -1,8 +1,18 @@
+/**
+ * Created by marinacalin on 5/16/17.
+ */
 public class Point {
 
-	public static void main(String[] args) {
+    public double x;
+    public double y;
 
-	System.out.println("This is the first sentence in a program called Point");
-	}
+    public Point(double arg1, double arg2) {
+        this.x = arg1;
+        this.y = arg2;
+    }
+
+    public double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+    }
 
 }
---
+++ playground/src/main/java/ProjectA.java
@@ -0,0 +1,12 @@
+public class ProjectA {
+
+	public static void main(String[] args) {
+
+	Point p1 = new Point(5,3);
+	Point p2 = new Point(7,4);
+	System.out.println("The distance between two points is " + p1.distance(p1,p2));
+	}
+
+
+
+}
